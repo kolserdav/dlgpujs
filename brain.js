ab3a533b3c70e182a21e83e27f7c5f9a0a71ff5e
kolserdav <serega12101983@gmail.com>
2019-04-22 18:01:07 +0700

    Socket io

--- brain.js
+++ brain.js
@@ -1,55 +1,74 @@
 const brain = require('brain.js');
 const nj = require('numjs');
 const jimp = require('jimp');
-const http = require('http');
-var GPU = require("gpu.js").GPU;
-var gpu = new GPU();
+const GPU = require("gpu.js").GPU;
+const gpu = new GPU();
+const fs = require('fs');
+const io = require('socket.io').listen(3000);
+    
+    
+class Brain {
 
-const image = './public/static/images/puppy-1903313__340.jpg';
-const server = http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    jimp.read(image)
-    .then(image => {
-        const width = image.bitmap.width;
-        const heigth = image.bitmap.height;
-        let array = nj.uint8(image.bitmap.data.toJSON().data);
-        array = array.reshape(heigth, width, 4)
-        let list = array.tolist();
-        console.log(list.length)
-        console.log(list[1].length)
-        console.log(list[1][1].length)
-        var gpuKernel = gpu.createKernel(function(array) {
-            if (this.thread.z % 3 === 0){
-                return array[this.thread.z][this.thread.y][this.thread.x] * 2;
-            }
-            else {
-                return array[this.thread.z][this.thread.y][this.thread.x];
-            }
+    constructor(){
+
+        this.data = [];
+        const dataPack = () => {
+            let data = fs.readFileSync('./dataSets/training-a.csv').toString();
+            data = data.split('\n');
+            data.map(item => {
+                let elements = item.split(',');
+                this.data.push({
+                    fileName: elements[0],
+                    fullFileName: elements[1],
+                    id: elements[2],
+                    digit: elements[3],
+                    dbName: elements[6]
+                });
+            });
+        };
+        dataPack();
+
+        const imageOld = './public/static/images/puppy-1903313__340.jpg';
+        const image = `./dataSets/${this.data[1].dbName}/${this.data[1].fileName}`
+        io.sockets.on('connection', (socket) => {
+            socket.on('message', (data) => {
+                jimp.read(image)
+                .then(image => {
+                    const width = image.bitmap.width;
+                    const heigth = image.bitmap.height;
+                    let array = nj.uint8(image.bitmap.data.toJSON().data);
+                    array = array.reshape(heigth, width, 4);
+                    let list = array.tolist();
+                    var gpuKernel = gpu.createKernel(function(array) {
+                        if (this.thread.x !== 3){
+                            return array[this.thread.z][this.thread.y][this.thread.x];
+                        }
+                        else {
+                            return array[this.thread.z][this.thread.y][this.thread.x];
+                        }
+                    })
+                        .setOutput([4, width, heigth]);
+                    list = gpuKernel(list);
+                    let array2 = nj.uint8(list);
+                
+                    array = array.reshape(heigth * width * 4)
+                    array2 = array2.reshape(heigth * width * 4)
+                    socket.json.send({
+                        data: array2,
+                        dataPack: this.data,
+                        width: width,
+                        height: heigth
+                    }); 
+                })
+                .catch(err => console.error(err))
+            });
+        });
+        io.sockets.on('disconnect', () => {
+            const time = (new Date).toLocaleTimeString();
+            io.sockets.json.send({'event': 'userSplit'});
         })
-            .setOutput([4, width, heigth]);
-        list = gpuKernel(list);
-        let array2 = nj.uint8(list);
-    
-        array = array.reshape(heigth * width * 4)
-        array2 = array2.reshape(heigth * width * 4)
-        console.log(list.length)
-        console.log(list[1].length)
-        console.log(list[1][1].length)
-        res.end(JSON.stringify({
-            data: array2,
-            width: width,
-            height: heigth
-        })) 
-    })
-    .catch(err => console.error(err))
-    
-})
-server.listen(3000)
+        
+    }
+}
 
-const myFunc = gpu.createKernel(function() {
-    return this.thread.x;
-}).setOutput([4, 3, 2]);
-
-console.log(myFunc());
-
-
+const b = new Brain();
--- public/module.js
+++ public/module.js
@@ -1,23 +1,41 @@
 
 
+
 const canvas = document.querySelector('#canvas');
 const canvas2 = document.querySelector('#canvas2');
 let ctx = canvas.getContext('2d');
 let ctx2 = canvas2.getContext('2d');
 
+class Module {
 
-const button = document.querySelector('#button');
-button.addEventListener('click', () => {
-    fetch('http://localhost:3000')
-        .then(r => r.json())
-        .then(data => {
-            console.log(data.data)
+    constructor(){
+        this.socket = io('http://localhost');
+        this.socket.on('connect', function(){
+            console.log('connected');
+        });
+        this.socket.on('message', function(data){
+            console.log(data);
+            console.log(data.dataPack)
             var dataImage = ctx.createImageData(data.width, data.height);
             const array = data.data.split(',');
             array.map((item, index) => {
                 dataImage.data[index] = parseInt(item);
             });
             ctx2.putImageData(dataImage, 0, 0);
+        });
+        this.socket.on('disconnect', function(){
+            console.warn('Disconnected!');
+        });
+
+
+        const button = document.querySelector('#button');
+        button.addEventListener('click', () => {
+            this.socket.send({
+                request: 0,
+                message: 'Start train'
+            });
         })
-        .catch(err => console.log(err))
-})
+    }
+}
+
+const m = new Module();
